8fa4107c61636b9356dc554a84420290c455c4c5
quanxtruong <cunqtran@gmail.com>
2024-04-22 00:20:04 -0500

    done for now!

--- app/src/main/java/palm/workshop/TextPrompts.java
+++ app/src/main/java/palm/workshop/TextPrompts.java
@@ -17,21 +17,23 @@ public class TextPrompts {
             .build();
         
         String context = "You find yourself in the middle of the forest, with a sword, a bow, " +
-        "or a magical staff in front of you\n What do you do?";
-        System.out.println("Welcome to this Text Adventure Game!");
+        "or a magical staff in front of you\nWhat do you do?";
+        System.out.println("\nWelcome to this Text Adventure Game!");
         System.out.println(context);
 
         boolean AOK = true;
         Scanner scan = new Scanner(System.in);
         while (AOK) {
+            System.out.print("\nAction: ");
             String userInput = scan.nextLine();
             if (userInput.toString().equals("quit")) {
                 AOK = false;
                 System.out.println("See you next time adventurer!");
             } else {
                 Response<String> response = model.generate("Based on this context: " + context + 
-                            ",continue the next few sentences in the story with this response: " + userInput.toString());
-                context = response.content();
+                            ", continue the story with an only one sentence scenario and then ask only one question, dungeons and dragons style" 
+                            + userInput.toString());
+                context = "\n" + response.content();
                 System.out.println(context); 
             }
         }
